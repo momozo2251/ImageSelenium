d31c2f001a8cef068a87de70fc51c79dbcaf1e99
ahegaowpeace <arduino0202@gmail.com>
2021-09-04 19:35:30 +0900

    ChromeDriverのパスを修正

--- image.py
+++ image.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -7,8 +8,8 @@ from selenium.webdriver.chrome.options import Options
 FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "image/screen.png")
 
 # set driver and url
-driver = webdriver.Chrome('./chromedriver')
-url = 'https://www.rakuten.co.jp/'
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+url = 'https://www.google.com/search?q=%E8%AA%BF%E5%B8%83+%E5%A4%A9%E6%B0%97&rlz=1C5CHFA_enJP928JP928&oq=%E8%AA%BF%E5%B8%83%E3%80%80%E5%A4%A9%E6%B0%97&aqs=chrome..69i57j0i512l9.4015j1j7&sourceid=chrome&ie=UTF-8'
 driver.get(url)
 
 # get width and height of the page
